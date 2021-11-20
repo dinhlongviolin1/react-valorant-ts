e9120e7989095b9878a6cb638ca21cf389cef81a
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-11-20 22:16:16 +0700

    update page size

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -20,7 +20,7 @@ export default function Store(props: AppProps) {
     return (
         <div>
             <nav className="bg-gray-800 shadow-sm sticky top-0 z-50">
-                <div className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div className="relative flex items-center justify-between h-16">
                         <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="flex-shrink-0 flex items-center cursor-pointer" onClick={cleanUpAndGoBack}>
@@ -43,7 +43,7 @@ export default function Store(props: AppProps) {
                     </div>
                 </div>
             </nav>
-            <div className="max-w-6xl mx-auto px-3 sm:px-6 lg:px-8 mt-4 sm:mt-5 lg:mt-6">
+            <div className="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8 mt-4 sm:mt-5 lg:mt-6">
                 <Carousel
                     className="rounded-sm"
                     autoPlay={true}
